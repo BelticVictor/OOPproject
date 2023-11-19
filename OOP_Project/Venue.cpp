43d2f80f21940567730a2eeacf8873bffb530d85
Beltic Gabriel <belticvictor22@stud.ase.ro>
2023-11-19 18:26:23 +0200

    ditto

--- OOP_Project/Venue.cpp
+++ OOP_Project/Venue.cpp
@@ -5,36 +5,69 @@
 using namespace std;
 
 class Venue {
-	char* name;
+	char* name=nullptr;
 	int maxSeats;
 	int rows;
-	int* seats_per_row; //seats_per_row[i]=number of seats on row i
+	int* seats_per_row=nullptr; //seats_per_row[i]=number of seats on row i
+	//dont forget to add zones
+
 public:
+
+	//constructor
+
 	Venue(char* _name, int _maxSeats, int _rows, int* _seats_per_row) : maxSeats(_maxSeats) {
 		this->set_name(_name);
 		this->set_rows(_rows, _seats_per_row);
 	}
 
-	//setters
+	//default constructor
+	
+	Venue() {
+		this->maxSeats = 0;
+		this->rows = 0;
 
-	void set_maxSeats(int _maxSeats) {
-		this->maxSeats = _maxSeats;
+		this->name = new char[strlen("John Doe") + 1];
+		strcpy_s(this->name, strlen("John Doe") + 1, "John Doe");
+
+		this->seats_per_row = new int[1];
+		this->seats_per_row[0] = 0;
 	}
 
-	void set_name(char* _name) {
-		this->name = new char[strlen(_name) + 1];
-		strcpy_s(this->name, strlen(_name) + 1, _name);
-	}
+	//copy constructor
 
-	void set_rows(int _rows, int* _seats_per_row) {
-		this->rows = _rows;
-		this->seats_per_row = new int[_rows + 1];
-		for (int i = 0; i < _rows; i++) {
-			this->seats_per_row[i] = _seats_per_row[i];
+	Venue(const Venue& copy) : maxSeats(copy.maxSeats), rows(copy.rows) {
+		this->name = new char[strlen(copy.name) + 1];
+		strcpy_s(this->name, strlen(copy.name) + 1, copy.name);
+
+		this->seats_per_row = new int[copy.rows + 1];
+		for (int i = 0; i < copy.rows; i++) {
+			this->seats_per_row[i] = copy.seats_per_row[i];
 		}
 	}
 
+	//setters
 
+	void set_maxSeats(int _maxSeats) {
+		if(_maxSeats > 0)
+			this->maxSeats = _maxSeats;
+	}
+
+	void set_name(char* _name) {
+		if (_name != nullptr) {
+			this->name = new char[strlen(_name) + 1];		
+			strcpy_s(this->name, strlen(_name) + 1, _name);
+	    }
+	}
+
+	void set_rows(int _rows, int* _seats_per_row) {
+		if (_rows > 0 && _seats_per_row != nullptr) {
+			this->rows = _rows;
+			this->seats_per_row = new int[_rows + 1];		
+			for (int i = 0; i < _rows; i++) {			
+				this->seats_per_row[i] = _seats_per_row[i];
+		    }
+	    }
+	}
 
 	//getters
 
@@ -49,4 +82,5 @@ public:
 		delete[] this->name;
 		delete[] this->seats_per_row;
 	}
+
 };
